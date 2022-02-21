e54b74af83870d1f957f3267c503ade5c7e326ec
Arthur Ammeux <80512684+ArthurAmmeux@users.noreply.github.com>
2022-02-21 15:08:10 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+import servo_driver as sd
+
+SHOULDER_PIN = 13
+ELBOW_PIN = 19
+HAND_PIN = 26
+
+
+def main():
+    s_pwm, e_pwm, h_pwm = sd.initialize(SHOULDER_PIN, ELBOW_PIN, HAND_PIN)
+    line = [[0.5, 0.5], [5, 0.5], [5, 5], [0.5, 5], [0.5, 0.5]]  # Draws a square
+    instructions = sd.points_to_instructions(line)  # Generate instructions
+    sd.plot_instructions(instructions, s_pwm, e_pwm, h_pwm)  # Plots instructions
+    time.sleep(2)
+    sd.stop_plot(s_pwm, e_pwm, h_pwm)  # Stop the plotter
+
+
+if __name__ == "__main__":
+    main()
---
+++ servo_driver.py
@@ -0,0 +1,129 @@
+import RPi.GPIO as GPIO
+import time
+import math
+
+SERVO_MIN = 2  # Duty cycle corresponding to 0° position of the servo
+SERVO_MAX = 12  # Duty cycle corresponding to 180° position of the servo
+
+# Cartesian coordinate limitations of the physical canvas
+X_MIN = 10
+Y_MIN = 30
+X_MAX = 100
+Y_MAX = 100
+
+L1 = 80  # Shoulder to elbow distance
+L2 = 85  # Elbow to hand distance
+# Distance squared for readability and optimization
+L1_sqr = L1**2
+L2_sqr = L2**2
+
+
+def initialize(shoulder_pin, elbow_pin, hand_pin):
+    # Initialize GPIO pins and all PWMs
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(shoulder_pin, GPIO.OUT)
+    GPIO.setup(elbow_pin, GPIO.OUT)
+    GPIO.setup(hand_pin, GPIO.OUT)
+    shoulder_pwm = GPIO.PWM(shoulder_pin, 50)
+    elbow_pwm = GPIO.PWM(elbow_pin, 50)
+    hand_pwm = GPIO.PWM(hand_pin, 50)
+    # Start PWM signal
+    shoulder_pwm.start(0)
+    elbow_pwm.start(0)
+    hand_pwm.start(0)
+    time.sleep(0.1)
+    # Move to initial position
+    shoulder_pwm.pwm.ChangeDutyCycle(2)
+    elbow_pwm.ChangeDutyCycle(2)
+    hand_pwm.ChangeDutyCycle(2)
+    time.sleep(0.3)
+    # Stop the servos in initial position (prevents twitching)
+    shoulder_pwm.ChangeDutyCycle(0)
+    elbow_pwm.ChangeDutyCycle(0)
+    hand_pwm.ChangeDutyCycle(0)  # pen is up
+    return shoulder_pwm, elbow_pwm, hand_pwm  # Returns PWM objects to be used to control the servos
+
+
+def cartesian_to_servo(x, y):
+    # coordinates relative to first servo (rather than relative to bottom left of the canvas)
+    x_ = x - X_MIN
+    y_ = y - Y_MIN
+    # Check if coordinates are plottable
+    assert 0 < x_ <= X_MAX - X_MIN
+    assert 0 < y_ <= Y_MAX - Y_MIN
+    r_sqr = x_**2 + y_**2  # r^2 in polar coordinates
+    alpha = math.atan(y_/x_)  # angle in polar coordinates
+    h = math.sqrt(L1_sqr - (r_sqr + L1_sqr - L2_sqr)**2/(4 * r_sqr))  # height of the L1, L2, r triangle
+    alpha_plus = math.asin(h/L1)
+    beta_plus = math.asin(h/L2)
+    theta = 2 * math.pi - (alpha + alpha_plus)  # shoulder servo angle
+    phi = alpha_plus + beta_plus  # elbow servo angle
+    return theta, phi
+
+
+def servo_to_dc(theta, phi):
+    #  Map theta, phi from [0-pi] to [2-12]% SG90 duty cycle range
+    shoulder_dc = theta*10/math.pi + 2
+    elbow_dc = phi*10/math.pi + 2
+    return shoulder_dc, elbow_dc
+
+
+def points_to_instructions(points):
+    instructions = []
+    for point in points:
+        theta, phi = cartesian_to_servo(point[0], point[1])
+        sdc, edc = servo_to_dc(theta, phi)
+        instructions.append([sdc, edc])
+    return instructions
+
+
+def plot_instructions(instructions, shoulder_pwm, elbow_pwm, hand_pwm):
+    # move pen to first point
+    shoulder_pwm.ChangeDutyCycle(instructions[0][0])
+    elbow_pwm.ChangeDutyCycle(instructions[0][1])
+    # Wait for movement completion
+    time.sleep(0.3)
+    # Prevents twitching
+    shoulder_pwm.ChangeDutyCycle(0)
+    elbow_pwm.ChangeDutyCycle(0)
+
+    # move pen down
+    hand_pwm.ChangeDutyCycle(10)
+    time.sleep(0.5)  # Wait for pen to be down and steady
+
+    for instruction in instructions[1:]:
+        sdc = instruction[0]
+        edc = instruction[1]
+        # Move to new point
+        shoulder_pwm.ChangeDutyCycle(sdc)
+        elbow_pwm.ChangeDutyCycle(edc)
+        # Wait for movement completion
+        time.sleep(0.3)
+        # Prevents twitching
+        shoulder_pwm.ChangeDutyCycle(0)
+        elbow_pwm.ChangeDutyCycle(0)
+
+    # move pen up
+    hand_pwm.ChangeDutyCycle(2)
+    time.sleep(0.5)  # Wait for pen to be up
+
+
+def stop_plot(shoulder_pwm, elbow_pwm, hand_pwm):
+    # Move to initial position
+    shoulder_pwm.pwm.ChangeDutyCycle(2)
+    elbow_pwm.ChangeDutyCycle(2)
+    hand_pwm.ChangeDutyCycle(2)
+    time.sleep(0.3)
+    # Stop the servos in initial position (prevents twitching)
+    shoulder_pwm.ChangeDutyCycle(0)
+    elbow_pwm.ChangeDutyCycle(0)
+    hand_pwm.ChangeDutyCycle(0)  # pen is up
+
+    # Stop PWMs
+    shoulder_pwm.stop()
+    elbow_pwm.stop()
+    hand_pwm.stop()
+
+    # Clear GPIO
+    GPIO.cleanup()
+
